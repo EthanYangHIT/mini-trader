6c1b3e72492446307c4ffcf8593722106b01aa67
Ethan <yangyusenhit@gmail.com>
2025-06-28 17:47:54 +0800

    ci: server path

--- playwright.config.ts
+++ playwright.config.ts
@@ -13,7 +13,7 @@ export default defineConfig({
   reporter: 'html',
   use: {
     trace: 'on-first-retry',
-    baseURL: 'http://localhost:5173',
+    baseURL: 'http://localhost:8080/mini-trader/',
   },
   projects: [
     {
