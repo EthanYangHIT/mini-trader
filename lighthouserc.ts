d5a17d98fcf81bcde806ced7cc99500e82ae1c88
Ethan <yangyusenhit@gmail.com>
2025-06-28 19:11:03 +0800

    ci: lighthouse stand

--- lighthouserc.ts
+++ lighthouserc.ts
@@ -8,14 +8,19 @@ const config = {
     },
     assert: {
       assertions: {
-        'categories:performance': ['warn', { minScore: 0.8 }],
-        'categories:accessibility': ['error', { minScore: 0.9 }],
-        'categories:best-practices': ['warn', { minScore: 0.8 }],
-        'categories:seo': ['warn', { minScore: 0.8 }],
+        'categories:performance': ['warn', { minScore: 0.7 }],
         'first-contentful-paint': ['warn', { maxNumericValue: 2000 }],
         'largest-contentful-paint': ['warn', { maxNumericValue: 4000 }],
         'cumulative-layout-shift': ['warn', { maxNumericValue: 0.1 }],
         'total-blocking-time': ['warn', { maxNumericValue: 300 }],
+        'max-potential-fid': ['warn', { maxNumericValue: 300 }],
+        'categories:accessibility': ['warn', { minScore: 0.7 }],
+        'aria-allowed-attr': ['warn', { minScore: 0.5 }],
+        'button-name': ['warn', { minScore: 0.5 }],
+        'categories:best-practices': ['warn', { minScore: 0.6 }],
+        'csp-xss': ['warn', { minScore: 0.1 }],
+        'render-blocking-resources': ['warn', { maxLength: 3 }],
+        'categories:seo': ['warn', { minScore: 0.8 }],
       },
     },
     upload: {
